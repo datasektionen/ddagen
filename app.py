b186127721bee2d2ea86deff6ee604617e1cd243
Andreas Holmgren <aho3@kth.se>
2017-09-13 20:17:18 +0200

    Add redirects for Visma and EPC

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
 	return flask.render_template("index.html")
 
+@app.route("/epc")
+def epc():
+	return flask.redirect("https://goo.gl/forms/yaSxeVi0EoFXYtYj1", code=302)
+
+@app.route("/visma")
+def visma():
+	return flask.redirect("https://goo.gl/forms/fvXTY7HovErr8WxA2", code=302)
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
