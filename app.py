9c7f0a258fbe33d4405dd38773409c98a64a5817
Sabrina Chowdhury <sabcho@kth.se>
2018-03-04 15:16:14 +0100

    Collection of work done in 2018

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
 	return flask.render_template("index.html")
 
-@app.route("/epc")
+@app.route("/sok")
 def epc():
-	return flask.redirect("https://goo.gl/forms/yaSxeVi0EoFXYtYj1", code=302)
+	return flask.redirect("https://docs.google.com/forms/d/e/1FAIpQLSeWdTma7qprNi-emaCIejwYhTpiSxHldbE7y_Yc_c0ri6S0Gg/viewform", code=302)
 
 @app.route("/visma")
 def visma():
@@ -18,6 +18,14 @@ def visma():
 @app.route("/sittningen")
 def sittning():
 	return flask.redirect("https://docs.google.com/forms/d/e/1FAIpQLSdwurt5xIhaUo6Tr6vfd6wmz1GgEj-nZggcrRYMkxxekqyLcA/viewform", code=302)
+
+@app.route("/yelp")
+def yelp():
+	return flask.redirect("https://goo.gl/forms/1WyVIE4BSL1RfQhh2", code=302)
+
+@app.route("/katalog")
+def kat():
+	return flask.redirect(flask.url_for('static', filename='katalogen.pdf'), code=302)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
