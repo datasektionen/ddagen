885ec3068fede2103ebd129a55739d5e0fa99aaa
Oskar Strömberg <oskstr@kth.se>
2019-02-21 22:57:09 +0100

    Handle 404 and 500 errors

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/foretag')
 def foretag():
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLSfqnMtyUMAc4IPxwAKdv5vKGR-PLam4XMqNwWQVyL9cl_Pt5w/closedform', code=302)
@@ -15,6 +14,14 @@ def foretag():
 @app.route('/industry')
 def industry():
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLSe_UJAnD-3Anweftecf3iOo_MBLLuLt1bBMHqI5bGNYFk90BQ/closedform', code=302)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
