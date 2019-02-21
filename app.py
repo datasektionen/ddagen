2b9808ffe465225fd4623c2941fc7535a714cd70
Oskar Strömberg <oskstr@kth.se>
2019-02-21 22:16:05 +0100

    Clean up and refactoring

--- app.py
+++ app.py
@@ -1,38 +1,20 @@
-import flask
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return flask.render_template("placeholder_index.html")
+    return render_template('index.html')
 
 
-@app.route("/sok")
-def sok():
-    return flask.redirect("https://goo.gl/forms/ZRtuxhYJIXfWjY1t1", code=302)
+@app.route('/foretag')
+def foretag():
+    return redirect('https://docs.google.com/forms/d/e/1FAIpQLSfqnMtyUMAc4IPxwAKdv5vKGR-PLam4XMqNwWQVyL9cl_Pt5w/closedform', code=302)
 
-
-# @app.route("/sittning")
-# def epc():
-# 	return flask.redirect("https://docs.google.com/forms/d/1P4eGEQpaWoOYlMeemEO0cJW9_-rQK1CEE5fFTFzJ_Xs/edit?usp=drive_open", code=302)
-#
-# @app.route("/visma")
-# def visma():
-# 	return flask.redirect("https://goo.gl/forms/fvXTY7HovErr8WxA2", code=302)
-#
-# @app.route("/sittningen")
-# def sittning():
-# 	return flask.redirect("https://docs.google.com/forms/d/e/1FAIpQLSdwurt5xIhaUo6Tr6vfd6wmz1GgEj-nZggcrRYMkxxekqyLcA/viewform", code=302)
-#
-# @app.route("/yelp")
-# def yelp():
-# 	return flask.redirect("https://goo.gl/forms/1WyVIE4BSL1RfQhh2", code=302)
-#
-# @app.route("/katalog")
-# def kat():
-# 	return flask.redirect(flask.url_for('static', filename='katalog.pdf'), code=302)
+@app.route('/industry')
+def industry():
+    return redirect('https://docs.google.com/forms/d/e/1FAIpQLSe_UJAnD-3Anweftecf3iOo_MBLLuLt1bBMHqI5bGNYFk90BQ/closedform', code=302)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
