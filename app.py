69f2e7672f5aef85b28f6915b24dcc0b7994f27b
Victor Sellstedt <victor.sellstedt@gmail.com>
2019-04-12 16:55:14 +0200

    Added support for changing language

--- app.py
+++ app.py
@@ -5,27 +5,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('SV/index.html')
+    language = request.args.get('language')
+    if language == 'en':
+        return render_template('EN/index.html')
+    else:
+        return render_template('SV/index.html')
 
 
 @app.route('/companies')
 def companies():
-    return render_template('SV/companies.html')
+    language = request.args.get('language')
+    if language == 'en':
+        return render_template('EN/companies.html')
+    else:
+        return render_template('SV/companies.html')
 
 
 @app.route('/visitors')
 def visitors():
+    # TODO Add visitors page
     return render_template('SV/index.html')
 
 
 @app.route('/contact')
 def contact():
+    # TODO Add contact page
     return render_template('SV/contact.html')
 
 
 @app.route('/faq')
 def faq():
-    return render_template('SV/faq.html')
+    language = request.args.get('language')
+    if language == 'en':
+        return render_template('EN/faq.html')
+    else:
+        return render_template('SV/faq.html')
 
 
 @app.route('/katalog2018')
