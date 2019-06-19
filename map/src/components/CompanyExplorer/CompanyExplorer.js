6b7df26ed794f3039df1d9c1c9059d858ade23e1
Victor Sellstedt <victor.sellstedt@gmail.com>
2019-06-19 22:52:19 +0200

    Restructured project to be component-centered

---
+++ map/src/components/CompanyExplorer/CompanyExplorer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './CompanyExplorer.css';
+import Map from '../Map/Map'
+
+class CompanyExplorer extends Component {
+  render() {
+    return (
+      <Map />
+    )
+  }
+}
+
+export default CompanyExplorer;
