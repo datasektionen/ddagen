4f2e304ce8b506e6ac5cefc5e0576fe31af1af3d
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-09-24 15:56:43 +0200

    Forgot quotation marks

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -6,6 +6,6 @@ eval `ssh-agent -s`
 ssh-add - <<< "$SSH_PRIVATE_KEY"
 ssh-keyscan sips.datasektionen.se >> ~/.ssh/known_hosts
 
-GIT_COMMAND=git push dokku@sips.datasektionen.se:ddagen-development develop:master
+GIT_COMMAND="git push dokku@sips.datasektionen.se:ddagen-development develop:master"
 
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 22" $GIT_COMMAND
