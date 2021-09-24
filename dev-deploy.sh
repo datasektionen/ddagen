c43f18d43bb5c739d8cdeadceac853ffc31c30e7
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-09-24 15:37:17 +0200

    Dev deploy script

---
+++ dev-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setup the SSH environment
+mkdir -p ~/.ssh
+eval `ssh-agent -s`
+ssh-add - <<< "$SSH_PRIVATE_KEY"
+ssh-keyscan sips.datasektionen.se >> ~/.ssh/known_hosts
+
+GIT_COMMAND=git push dokku@sips.datasektionen.se:ddagen-development develop:master
+
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $SSH_PORT" $GIT_COMMAND
