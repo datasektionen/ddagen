46e048a493c7f456492d872c1350ba08db4c06cc
Gustav Ekner <gustav.ekner@gmail.com>
2022-03-11 11:14:25 +0100

    New design

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,125 +1,118 @@
-// Skrivet på formatet [Namn, länk till bild från "static/img/", mail, Svensk titel, Engelsk titel]
-// Scriptet körs när contact.html laddas in.
+// När denna "funktion" körs i kombination med "new" kommer den fungera som en 
+// konstruktor och skapa ett nytt Person-objekt.
+function Person(name, img, email, sv_title, en_title) {
+    this.name = name;
+    this.img = img;
+    this.email = email;
+    this.title = {
+        sv: sv_title,
+        en: en_title
+    };
+}
 
-dda = [
-    ["Carl Chemnitz", "maskot-bilder/alpa02-dda.png", "ansvarig@ddagen.se", "D-Dagenansvarig", "Project Leader"], 
-    ["Kevin Wenström", "maskot-bilder/alpa02-dda.png", "ansvarig@ddagen.se", "D-Dagenansvarig", "Project Leader"]
-];
-
-fin = [
-    ["Felix Almay", "maskot-bilder/alpa02-ekonomi.png", "ekonomi@ddagen.se", "Kassör", "Treasurer"],
-    ["Axel Johansson", "maskot-bilder/alpa02-ekonomi.png", "v.ekonomi@ddagen.se", "Vice kassör", "Vice-Treasurer"]
-];
-
-sales = [
-    ["Carl Peterson", "maskot-bilder/alpa02-säljansvarig.png", "carl.peterson@ddagen.se", "Säljare", "Sales Associate"],
-    ["Karl Lindblad", "maskot-bilder/alpa02-sälj.png", "karl.lindblad@ddagen.se", "Säljare", "Sales Associate"],
-    ["Vilmer Jonsson", "maskot-bilder/alpa02-sälj.png", "vilmer.jonsson@ddagen.se", "Säljare", "Sales Associate"],
-    ["Johan Abdi", "maskot-bilder/alpa02-tryck-sponsor.png", "sponsorship@ddagen.se", "Sponsansvarig", "Head of Sponsorship"]
-];
-
-fair = [
-    ["Tobias Hansson", "maskot-bilder/alpa02-personalansvarig.png", "fair-temp@ddagen.se", "Mässansvarig", "Head of Staff"],
-    ["Emil Hultcrantz", "maskot-bilder/alpa02-lokalansvarig.png", "facility@ddagen.se", "Lokalansvarig", "Head of Venue"],
-    ["David Peilitz", "maskot-bilder/alpa02-logistik.png", "logistics@ddagen.se", "Logistikansvarig", "Head of Logistics"],
-    ["Rafael Bechara", "maskot-bilder/alpa02-lounge.png", "lounge@ddagen.se", "Loungeansvarig", "Head of Lounge"]
-];
-
-pr = [
-    ["Mortada Nasser", "maskot-bilder/alpa02-pransvarig.png", "pr@ddagen.se", "PR-ansvarig", "Head of PR"],
-    ["Gustav Ekner", "maskot-bilder/alpa02-webansvarig.png", "web@ddagen.se", "Webbansvarig", "Webmaster"],
-    ["Joseph Karroum", "maskot-bilder/alpa02-grafik.png", "art@ddagen.se", "Art Director", "Art Director"],  
-];
-
-events = [
-    ["Amanda Berg", "maskot-bilder/alpa02-fest.png", "fest-temp@ddagen.se", "Festansvarig", "Head of Events"]
-];
-
-/**
- * Lägger till personerna definerade i listorna ovan till "Kontakta oss" sidan.
- * 
- * @param lang "swe" eller "eng" beroende på språk 
- */
-function addContactInfo(lang) {
-    try {        
-        appendElem("dda", lang, dda[0]);
-        appendElem("dda", lang, dda[1]);
-        
-        appendElem("fin", lang, fin[0]);
-        appendElem("fin", lang, fin[1]);
-        
-        appendElem("sales", lang, sales[0]);
-        appendElem("sales", lang, sales[1]);
-        appendElem("sales", lang, sales[2]);
-        appendElem("sales", lang, sales[3]);
-
-        appendElem("fair", lang, fair[0]);
-        appendElem("fair", lang, fair[1]);
-        appendElem("fair", lang, fair[2]);
-        appendElem("fair", lang, fair[3]);
-        
-        appendElem("pr", lang, pr[0]);
-        appendElem("pr", lang, pr[1]);
-        appendElem("pr", lang, pr[2]);
-
-        appendElem("event", lang, events[0]);
-    } catch (error) {
-        console.log(error);
+// För att lägga till/ta bort medlemmar är det bara att ändra i den här listan,
+// inga ändringar i HTML-koden behöver göras.
+var pageData = [
+    {
+        title: {sv: "Projektledare", en: "Project Managers"},
+        persons: [
+            new Person("Carl Chemnitz", "maskot-bilder/alpa02-dda.png", "ansvarig@ddagen.se", "D-Dagenansvarig", "Project Leader"), 
+            new Person("Kevin Wenström", "maskot-bilder/alpa02-dda.png", "ansvarig@ddagen.se", "D-Dagenansvarig", "Project Leader")
+        ]
+    },
+    {
+        title: {sv: "Ekonomi", en: "Finance"},
+        persons: [
+            new Person("Felix Almay", "maskot-bilder/alpa02-ekonomi.png", "ekonomi@ddagen.se", "Kassör", "Treasurer"),
+            new Person("Axel Johansson", "maskot-bilder/alpa02-ekonomi.png", "v.ekonomi@ddagen.se", "Vice kassör", "Vice-Treasurer")
+        ]
+    },
+    {
+        title: {sv: "Sälj", en: "Sales"},
+        persons: [
+            new Person("Carl Peterson", "maskot-bilder/alpa02-säljansvarig.png", "carl.peterson@ddagen.se", "Säljare", "Sales Associate"),
+            new Person("Karl Lindblad", "maskot-bilder/alpa02-sälj.png", "karl.lindblad@ddagen.se", "Säljare", "Sales Associate"),
+            new Person("Vilmer Jonsson", "maskot-bilder/alpa02-sälj.png", "vilmer.jonsson@ddagen.se", "Säljare", "Sales Associate"),
+            new Person("Johan Abdi", "maskot-bilder/alpa02-tryck-sponsor.png", "sponsorship@ddagen.se", "Sponsansvarig", "Head of Sponsorship")
+        ]
+    },
+    {
+        title: {sv: "Mässa", en: "Fair"},
+        persons: [
+            new Person("Tobias Hansson", "maskot-bilder/alpa02-personalansvarig.png", "fair-temp@ddagen.se", "Mässansvarig", "Head of Staff"),
+            new Person("Emil Hultcrantz", "maskot-bilder/alpa02-lokalansvarig.png", "facility@ddagen.se", "Lokalansvarig", "Head of Venue"),
+            new Person("David Peilitz", "maskot-bilder/alpa02-logistik.png", "logistics@ddagen.se", "Logistikansvarig", "Head of Logistics"),
+            new Person("Rafael Bechara", "maskot-bilder/alpa02-lounge.png", "lounge@ddagen.se", "Loungeansvarig", "Head of Lounge")
+        ]
+    },
+    {
+        title: {sv: "PR", en: "PR"},
+        persons: [
+            new Person("Mortada Nasser", "maskot-bilder/alpa02-pransvarig.png", "pr@ddagen.se", "PR-ansvarig", "Head of PR"),
+            new Person("Gustav Ekner", "maskot-bilder/alpa02-webansvarig.png", "web@ddagen.se", "Webbansvarig", "Webmaster"),
+            new Person("Joseph Karroum", "maskot-bilder/alpa02-grafik.png", "art@ddagen.se", "Art Director", "Art Director")
+        ]
+    },
+    {
+        title: {sv: "Fest", en: "Event"},
+        persons: [
+            new Person("Amanda Berg", "maskot-bilder/alpa02-fest.png", "fest-temp@ddagen.se", "Festansvarig", "Head of Events")
+        ]
     }
-}
+];
+
+var imgLocation = "/static/img/";
+
+// HTML-kod för elementet som innehåller en hel grupp, t.ex. Säljgruppen:
+var teamHTML = `
+    <div class="uk-card uk-card-default uk-card-body">
+        <h2></h2>
+        <div class="uk-grid-column-small uk-grid-row-large uk-child-width-1-4@s uk-text-center uk-flex-center" uk-grid>
+        </div>
+    </div>
+`;
+
+// HTML-kod för elementet som innehåller en person, t.ex. Säljansvarig:
+var personHTML = `
+    <div class="uk-card uk-card-default uk-card-body">
+        <img class="uk-border-circle" width="180px" style="height: 180px; object-fit: cover;" alt="" uk-img>
+        <h3 class="uk-margin-remove-bottom"></h3>
+        <h4 class="uk-margin-small"></h4>
+        <p class="uk-margin-remove-top"><p>
+    </div>
+`;
 
 /**
- * Creates a html element of a person
+ * Lägger till personerna definerade i listorna ovan till "Kontakta oss"-sidan.
  * 
- * @param person is a list with info about the person
- * @param titleLang is the language of the title, "swe" or "eng"
- * 
- * @returns a html element of a person
+ * @param lang "sv" eller "en" beroende på språk 
  */
-function getElement(person, titleLang) {
-    const elem = document.createElement("div");
-    elem.className = "portrait-photo";
-    
-    var img = document.createElement("img");
-    img.src = "static/img/" + person[1];
+ function addContactInfo(lang) {
+    for (i = 0; i < pageData.length; i++) {
+        var teamDiv = document.createElement("div");
+        teamDiv.innerHTML = teamHTML.trim();
+        var personContainer = teamDiv.children[0].children[1];
 
-    const name = ctxtelem("h3", person[0]); 
-    let titleID = 4;
-    if (titleLang === "swe") {
-        titleID = 3;
-    } 
-    const title = ctxtelem("h5", person[titleID]);         
-    var mail = ctxtelem("a", person[2]);
-    mail.href = "mailto:" +  person[2];
+        for (j = 0; j < pageData[i].persons.length; j++) {
+            var person = pageData[i].persons[j];
+            var personDiv = document.createElement("div");
+            personDiv.innerHTML = personHTML.trim();
 
-    elem.append(img);
-    elem.append(name);
-    elem.append(title);
-    elem.append(mail);
-    return elem;
-}
+            var img = personDiv.children[0].children[0];
+            var name = personDiv.children[0].children[1];
+            var title = personDiv.children[0].children[2];
+            var email = personDiv.children[0].children[3];
 
-/**
- * Creates a text element. e.g. <type>text</type>, <h1>title</h1>
- * 
- * @param type is the type of html element to create.
- * @param text is the text to put inside.
- * 
- * @returns a html element with text inside 
- */
-function ctxtelem(type, text) {
-    const elem = document.createElement(type);
-    elem.append(document.createTextNode(text));
-    return elem;
-}
+            img.setAttribute("data-src", imgLocation + person.img);
+            name.innerHTML = person.name;
+            title.innerHTML = person.title[lang];
+            email.innerHTML = "<a href='mailto:" + person.email + "'>" + person.email + "</a>";
 
-/**
- * Appends a person to specified div.
- * 
- * @param id is the id of the div to append the person to.
- * @param lang is the language to use
- * @param person is a list with the details
- */
-function appendElem(id, lang, person) {
-    document.getElementById(id).append(getElement(person, lang));
+            personContainer.appendChild(personDiv);
+        }
+
+        teamDiv.querySelector("h2").innerHTML = pageData[i].title[lang];
+
+        document.getElementById("main-grid").appendChild(teamDiv);
+    }
 }
