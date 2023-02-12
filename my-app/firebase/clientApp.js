a32fb226d09c7f19a54dacfc2aeebf7fa500e1ba
Ernest Umeh <66337956+ernum@users.noreply.github.com>
2023-02-12 21:05:09 +0100

    feat(ddagen): Add tailwind to project and create HTML form (#5)
    Co-authored-by: mathiasmagnusson <mathias@magnusson.space>

--- my-app/firebase/clientApp.js
+++ my-app/firebase/clientApp.js
@@ -1,18 +1,18 @@
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const clientCredentials = {
-    api: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectid: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKETddagen,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-if (!firebase.getApps.length)
-    firebase.initializeApp(clientCredentials);
+if (!firebase.apps.length)
+  firebase.initializeApp(clientCredentials);
 
 export default firebase;
 
--- my-app/src/locales/sv.js
+++ my-app/src/locales/sv.js
@@ -1,4 +1,13 @@
 export default {
     about: 'Om Oss',
-    home: 'Hem'
+    home: 'Hem',
+    companyForm: {
+        name: 'Företagsnamn',
+        orgNum: 'Organisationsnummer',
+        contact: 'Kontaktperson',
+        email: 'Meljadress',
+        invAddress: 'Faktureringsadress',
+        invEmail: 'Mejladress För Fakturering',
+        next: 'Nästa'
+    }
 };
