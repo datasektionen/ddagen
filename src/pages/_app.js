3ffccdb4b322dfe3fae0fafd09df83f8450ad996
Mathias Magnusson <32957299+mathiasmagnusson@users.noreply.github.com>
2023-02-14 11:56:24 +0100

    feat(ddagen): Add navbar to app (#3)
    Co-authored-by: Ernest Umeh <umeh@kth.se>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import Navbar from '@/components/Navbar';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Navbar />
+            <Component {...pageProps} />
+        </>
+    );
 }
