ea2b5f15e9772144df89f37e0ece3a718f0e2d52
Adam <adam.egnell@hotmail.com>
2023-02-24 13:31:42 +0100

    förbättra strukturen på mobil (inte figma design)

--- src/pages/f%C3%B6rf%C3%B6retag.tsx
+++ src/pages/f%C3%B6rf%C3%B6retag.tsx
@@ -7,15 +7,15 @@ export default function Companies() {
     return (
         <div className="bg-slate-700 flex h-min-screen flex-col items-center">
             <h1 className="text-cerise my-32 text-3xl">För Företag</h1>
-            <div className="grid grid-cols-2 gap-y-16 bg-white/50 w-4/5 h-30 text-slate-700 py-16 px-8 mb-32">
-                <div className="bg-white p-8 m-auto min-w-[20rem] z-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-16 bg-white/50 h-30 sm:w-4/5 py-16 sm:px-8 px-2 mx-2 mb-32">
+                <div className="bg-white p-6 sm:p-8 m-auto sm:min-w-[20rem] z-10">
                     <h2 className="uppercase text-cerise font-bold text-2xl pb-4">Om Mässan</h2>
                     <p>D-Dagen är Datasektionens årliga arbetsmarknadsdag. Det är ett heldagsevent där företag och studenter verksamma inom data och IT får en möjlighet att knyta kontakter och lära känna varandra. Detta skapar goda möjligheter för en mer direkt och personlig kontakt mellan företagsrepresentanter och studenter.</p>
                     <p className="pt-4">I år hålls mässan 11 oktober i THS kårhus, Nymble, som ligger på Drottning Kristinas väg 15-19 på KTH Campus Valhallavägen. Under dagen kommer det finnas en lounge för utställare, och på kvällen anordnas en bankett för att avsluta dagen.</p>
                 </div>
-                <div className="flex m-auto justify-self-end flex justify-end w-full"><img className="bg-black min-w-[20rem] right-0"src="\img\for-companies-pic1.svg" alt="picture of people talking" /></div>
+                <div className="flex m-auto justify-self-end flex justify-end w-full"><img className="bg-black sm:min-w-[30rem] right-0"src="\img\for-companies-pic1.svg" alt="picture of people talking" /></div>
                 <div className="flex mb-auto"><img className="bg-black "src="\img\for-companies-pic2.svg" alt="picture of people talking" /></div>
-                <div className="bg-cerise text-black p-8 mb-auto mt-8 min-w-[20rem] justify-self-end">
+                <div className="bg-cerise text-black p-8 mb-auto sm:mt-8 sm:min-w-[20rem] justify-self-end">
                     <h2 className="font-bold text-2xl pb-4">Intresserad?</h2>
                     <p className="pb-8">Är du intresserad av att ställa ut på D-dagen och knyta kontagt med tusentals studenter inom Data och IT? Klicka här nedan för att göra en intresseanmälan!</p>
                     <Link className="bg-white p-2.5 rounded-full text-cerise" href="/företagsanmälan">{t.nav.companyForm}</Link>
