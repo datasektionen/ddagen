77871d0641556f976b34d63d7b15789449b6a3bd
Mathias Magnusson <32957299+mathiasmagnusson@users.noreply.github.com>
2023-02-28 22:29:03 +0100

    feat(ddagen): Add a favicon and title (#22)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <link rel="icon" href="/img/favicon.ico" />
+                <title>D-Dagen 2023 | Konglig Datasektionens Arbetsmässodag</title>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
