6b94b53790d71de49553bcd293217a5105c28a4a
rikaznismi <58601215+rikaznismi@users.noreply.github.com>
2023-03-01 15:39:45 +0100

    fix(ddagen): Background image loading slowly (#25)
    Co-authored-by: mathiasmagnusson <mathias@magnusson.space>
    Co-authored-by: Ernest Umeh <umeh@kth.se>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="icon" href="/img/favicon.ico" />
-                <title>D-Dagen 2023 | Konglig Datasektionens Arbetsmässodag</title>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/img/favicon.ico" />
+        <title>D-Dagen 2023 | Konglig Datasektionens Arbetsmässodag</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
