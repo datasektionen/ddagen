9a43a8ac702dd2fb430e2d57d4b3af5650b90455
Ernest Umeh <66337956+ernum@users.noreply.github.com>
2023-03-05 13:47:18 +0100

    feat(app): Add email confirmation and update homepage image (#27)
    Co-authored-by: mathiasmagnusson <mathias@magnusson.space>

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -5,15 +5,12 @@ export default {
   },
   about: "About Us",
   home: {
-    introFirstBlock:
-      "It’s once again time for D-dagen, the largest job fair for Computer Science students in the Nordics.",
-    introSecondBlock:
-      "Are you studing computer science or IT and looking for your future work place, or do you represent a company and are looking for talented co-workers? In that case, D-dagen is the perfect opportunity for you to meet people from the industry and expand your network with valuable contacts! We’re striving to make this years fair the largest so far, with over 100 exhibitors and 3000 participants.",
+    introFirstBlock: "It’s once again time for D-dagen, the largest job fair for Computer Science students in the Nordics.",
+    introSecondBlock: "Are you studing computer science or IT and looking for your future work place, or do you represent a company and are looking for talented co-workers? In that case, D-dagen is the perfect opportunity for you to meet people from the industry and expand your network with valuable contacts! We’re striving to make this years fair the largest so far, with over 100 exhibitors and 3000 participants.",
     introSignOff: "Looking forward to meeting you!",
     introDDA: "Axel Johansson & Johan Abdi, Project Managers",
     representative: "Company Representative?",
-    representativeDescription:
-      "Do you want to reach thousands of civil engineering students at KTH? As an exhibitor at D-dagen you have thet opportunity to do so! Click down bellow to read more and register your interest.",
+    representativeDescription: "Do you want to reach thousands of civil engineering students at KTH? As an exhibitor at D-dagen you have thet opportunity to do so! Click down bellow to read more and register your interest.",
     representativeButton: "Interest Registration",
     stats: {
       firstPart: "100+ exhibitors",
@@ -23,27 +20,29 @@ export default {
   forCompanies: {
     title: "For Companies",
     aboutFair: "About the fair",
-    fairText1:
-      "D-Dagen is the Computer Science Chapter’s annual career fair. It is an all-day event where companies and students studying computer science and IT get the opportunity to connect and get to know each other. This creates good opportunities for a more direct and personal contact between company representatives and students.",
-    fairText2:
-      "This year the fair is held on October 12th in the THS student union building, Nymble, which is located at Drottning Kristinas väg 15-19 on the KTH Campus Valhallavägen. During the day there will be a lounge for exhibitors, and in the evening a banquet will be organized to end the day.",
+    fairText1: "D-Dagen is the Computer Science Chapter’s annual career fair. It is an all-day event where companies and students studying computer science and IT get the opportunity to connect and get to know each other. This creates good opportunities for a more direct and personal contact between company representatives and students.",
+    fairText2: "This year the fair is held on October 12th in the THS student union building, Nymble, which is located at Drottning Kristinas väg 15-19 on the KTH Campus Valhallavägen. During the day there will be a lounge for exhibitors, and in the evening a banquet will be organized to end the day.",
     intrestedTitle: "Interested?",
-    intrestedText:
-      "Are you interested in exhibiting on D-Dagen and connecting with thousands of students in Computer Science and IT? Click below to sign up!",
+    intrestedText: "Are you interested in exhibiting on D-Dagen and connecting with thousands of students in Computer Science and IT? Click below to sign up!",
     formButton: "Sign up",
   },
   companyForm: {
     title: "Sign up for D-Dagen",
-    name: "Company Name",
-    organizationNumber: "Organization Number",
-    email: "Email Address",
-    contactPerson: "Contact Person",
-    phoneNumber: "Phone Number",
+    fields: {
+      name: "Company Name",
+      organizationNumber: "Organization Number",
+      email: "Email Address",
+      contactPerson: "Contact Person",
+      phoneNumber: "Phone Number",
+    },
     confirm: "Send",
-    error: "Something went wrong! Try again or send us an email instead to",
+    error: {
+      db: "Something went wrong! Try again or send an email instead to",
+      email: "Your registration has been received, but we could not send a confirmation email to you.",
+      continue: "Continue anyway",
+    },
     organizationNumberLength: "The organization number must be 10 digits",
-    organizationNumberChecksum:
-      "Invalid check digit, check that you have entered the number correctly",
+    organizationNumberChecksum: "Invalid check digit, check that you have entered the number correctly",
   },
   postCompanyForm: {
     title: "Registration confirmed",
@@ -62,10 +61,37 @@ export default {
   },
   footer: {
     header: "Organized by Datasektionen",
-    about:
-      "Datasektionen, or the Computer Science Chapeter is a non-profit student chapter under THS which exists to give all CS students at KTH the best study time possible.",
+    about: "Datasektionen, or the Computer Science Chapeter is a non-profit student chapter under THS which exists to give all CS students at KTH the best study time possible.",
     contactHeader: "Contact",
     responsible: "Project Managers",
     salesGroup: "Sales Team",
+  },
+  email: {
+    subject: "Exhibitor Registration Confirmation",
+    body: (
+      companyName: string,
+      organizationNumber: string,
+      email: string,
+      contactPerson: string,
+      phoneNumber: string,
+    ) => `
+      <p>Hi!</p>
+      <p>We are pleased to confirm we have received your exhibitor registration.
+      You will be contacted by a salesperson with more information about the fair.</p>
+
+      <p>Here are the details of your registration:</p>
+      <ul>
+        <li>Company Name: ${companyName}</li>
+        <li>Organization Number: ${organizationNumber}</li>
+        <li>Email Address: ${email}</li>
+        <li>Contact Person: ${contactPerson}</li>
+        <li>Phone Number: ${phoneNumber}</li>
+      </ul>
+      <p>Let us know if any of the information above is incorrect.</p>
+      <p>Thank you for your registration and we look forward to a successful event.</p>
+
+      <p>Best regards,</p>
+      <p>Axel Johansson & Johan Abdi, Project Managers</p>
+    `,
   },
 };
