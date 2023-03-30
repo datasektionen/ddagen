1f3dc329399ab0ea7c8ee912fbf5eb5209fab2d6
Ernest Umeh <umeh@kth.se>
2023-03-30 16:03:39 +0200

    fix: Email address in company form

--- src/pages/f%C3%B6retagsanm%C3%A4lan.tsx
+++ src/pages/f%C3%B6retagsanm%C3%A4lan.tsx
@@ -7,9 +7,9 @@ function RegistrationConfirmation({ t }: { t: Locale["postCompanyForm"] }) {
         <div className="mx-auto flex flex-col items-center text-center py-40">
             <h1 className="md:max-w-2xl md:text-5xl mx-10 text-3xl text-cerise uppercase mb-24">{t.title}</h1>
             <h2 className="text-lg text-white w-2/3 uppercase mb-5">{t.subtitle}</h2>
-            <p className="max-w-[350px] w-2/3 text-[#a7a7a7] mb-14">{t.text} <a href="mailto:ansvarig@ddagen.se" className="text-cerise hover:underline">ansvarig@ddagen.se</a></p>
+            <p className="max-w-[350px] w-2/3 text-[#a7a7a7] mb-14">{t.text} <a href="mailto:sales@ddagen.se" className="text-cerise hover:underline">sales@ddagen.se</a></p>
 
-            <a href="mailto:ansvarig@ddagen.se" className="
+            <a href="mailto:sales@ddagen.se" className="
                 bg-cerise transition-transform hover:scale-110
                 text-white font-bold uppercase
                 py-2 px-4 rounded-full
