320a36b5cf656dc0f54d880a56108f61b4329794
rikaznismi <rikaznismi@hotmail.com>
2023-04-02 22:53:00 +0200

    fix: Current used table button is brightened up

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -158,22 +158,23 @@ export default function Faq() {
 
       {/*Dropdown table*/}
       <div className="h-full min-w-[200px] max-w-[1200px] w-full mt-[100px] px-[30px]">
-        <div className="h-[50px] bg-inherit flex flex-col sm:flex-row  justify-center items-center mb-[80px] px-[30px] sm:px-[0px]">
-          {/*Dropdown buttons*/}
+        {/*Dropdown buttons*/}
+        <div className="h-[50px] bg-inherit flex flex-col sm:flex-row  justify-center items-center mb-[80px] px-[30px] sm:px-[0px] ">
+          
           <div className="sm:w-full flex flex-row justify-center items-center">
-              <div className="sm:w-full text-center h-[50px] mr-[20px] sm:mr-[40px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer" onClick={() => setCurrentTable(0)}>
+              <div className={`${!(currentTable-0) ? 'brightness-100': 'brightness-75'} sm:w-full text-center h-[50px] mr-[20px] sm:mr-[40px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer`} onClick={() => setCurrentTable(0)}>
                 <p className="text-white text-[11px] sm:text-base break-words w-[100px] sm:w-auto"> {t.faq.box1}</p>
               </div>
-              <div className="sm:w-full text-center h-[50px] mr-[0px] sm:mr-[40px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer" onClick={() => setCurrentTable(2)}>
+              <div className={`${!(currentTable-2) ? 'brightness-100': 'brightness-75'} sm:w-full text-center h-[50px] mr-[0px] sm:mr-[40px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer`} onClick={() => setCurrentTable(2)}>
                 <p className="text-white text-[11px] sm:text-base break-words w-[100px] sm:w-auto"> {t.faq.box3}</p>
               </div>
           </div>
 
           <div className="sm:w-full flex flex-row justify-center items-center mt-[20px] sm:mt-[0px]">
-              <div className="sm:w-full text-center h-[50px]  mr-[20px] sm:mr-[40px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer" onClick={() => setCurrentTable(1)}>
+              <div className={`${!(currentTable-1) ? 'brightness-100': 'brightness-75'} sm:w-full text-center h-[50px]  mr-[20px] sm:mr-[40px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer`} onClick={() => setCurrentTable(1)}>
                 <p className="text-white text-[11px] sm:text-base break-words w-[100px] sm:w-auto"> {t.faq.box2}</p>
               </div>
-              <div className="sm:w-full text-center h-[50px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer" onClick={() => setCurrentTable(3)}>
+              <div className={`${!(currentTable-3) ? 'brightness-100': 'brightness-75'} sm:w-full text-center h-[50px] bg-slate-50 bg-opacity-20 border-[3px] border-cerise flex items-center justify-center cursor-pointer`} onClick={() => setCurrentTable(3)}>
                 <p className="text-white text-[11px] sm:text-base break-words w-[100px] sm:w-auto"> {t.faq.box4}</p>
               </div>
           </div>
