cfc53254960aa39921c51b7f831465da4157f477
rikaznismi <rikaznismi@hotmail.com>
2023-04-03 23:27:00 +0200

    Add: Generalised Table function

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,104 +1,30 @@
 import { useLocale } from "@/locales";
 import React, { useState } from "react";
 
-function Table1(stateAction: Array<[boolean, React.Dispatch<React.SetStateAction<boolean>>]>){
-
-  const t = useLocale()
-
+function Table(questions: Array<string>, answers: Array<string>, stateAction: Array<[boolean, React.Dispatch<React.SetStateAction<boolean>>]>){
+  
   return(
-  <div className="mt-[50px]">
+    <div className="mt-[50px]">
 
-    <button onClick={() => stateAction[0][1](!stateAction[0][0])} className="pl-[20px] text-left text-white h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise"> {t.faq.table1row1}</button>
-    {stateAction[0][0] ? (
-    <div className="pl-[20px] h-[200px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
-    <p className="text-white">{t.faq.table1text1}</p>
-    </div>) : null}
-
-    <button onClick={() => stateAction[1][1](!stateAction[1][0])} className="pl-[20px] text-left text-white h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise">{t.faq.table1row2}</button>
-    {stateAction[1][0] ? (
-    <div className="pl-[20px] h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
-    <p className="text-white">{t.faq.table1text2}</p>
-    </div>) : null}
-
-    <button onClick={() => stateAction[2][1](!stateAction[2][0])} className="pl-[20px] text-left text-white h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise">{t.faq.table1row3}</button>
-    {stateAction[2][0] ? (
-    <div className="pl-[20px] h-[130px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
-    <p className="text-white">{t.faq.table1text3}</p>
-    </div>) : null}
-
-</div>)
-}
-
-function Table2(stateAction: Array<[boolean, React.Dispatch<React.SetStateAction<boolean>>]>){
-
-  const t = useLocale()
-
-  return(
-  <div className="mt-[50px]">
-
-    <button onClick={() => stateAction[0][1](!stateAction[0][0])} id="accordion" className="pl-[20px] text-left text-white h-[85px] sm:h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise"> {t.faq.table2row1}</button>
-    {stateAction[0][0] ? (
-    <div className="pl-[20px] h-[150px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center ">
-    <p className="text-white">{t.faq.table2text1_1} <a className="text-cerise" href="mailto:sales@ddagen.se">sales@ddagen.se</a> {t.faq.table2text1_2}</p>
-    </div>) : null}
-
-    <button onClick={() => stateAction[1][1](!stateAction[1][0])} id="accordion" className="pl-[20px] text-left text-white h-[120px] sm:h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise">{t.faq.table2row2}</button>
-    {stateAction[1][0] ? (
-    <div className="pl-[20px] h-[200px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
-    <p className="text-white">{t.faq.table2text2}</p>
-    </div>) : null}
-
-</div>)
-}
-
-function Table3(stateAction: Array<[boolean, React.Dispatch<React.SetStateAction<boolean>>]>){
-
-  const t = useLocale()
-
-  return(
-  <div className="mt-[50px]">
-
-    <button onClick={() => stateAction[0][1](!stateAction[0][0])} id="accordion" className="pl-[20px] text-left text-white h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise"> {t.faq.table3row1}</button>
-    {stateAction[0][0] ? (
-    <div className="pl-[20px] h-[140px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center ">
-    <p className="text-white">{t.faq.table3text1_1} <a href={t.url.forCompany} target="blank" className="underline text-cerise">  {t.url.forCompany} </a>{t.faq.table3text1_2} <a className="text-cerise" href="mailto:sales@ddagen.se">sales@ddagen.se</a></p>
-    </div>) : null}
-
-    <button onClick={() => stateAction[1][1](!stateAction[1][0])} id="accordion" className="pl-[20px] text-left text-white h-[85px] sm:h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise">{t.faq.table3row2}</button>
-    {stateAction[1][0] ? (
-    <div className="pl-[20px] h-[260px] sm:h-[150px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
-    <p className="text-white">{t.faq.table3text2_1} <a href={t.url.companyForm} target="blank" className="underline text-cerise"> {t.url.companyForm}</a></p>
-    </div>) : null}
-
-    <button onClick={() => stateAction[2][1](!stateAction[2][0])} id="accordion" className="pl-[20px] text-left text-white h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise">{t.faq.table3row3}</button>
-    {stateAction[2][0] ? (
-    <div className="pl-[20px] h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
-    <p className="text-white">{t.faq.table3text3} <a className="text-cerise" href="mailto:ansvarig@ddagen.se">ansvarig@ddagen.se</a></p>
-    </div>) : null}
-
-    <button onClick={() => stateAction[3][1](!stateAction[3][0])} id="accordion" className="pl-[20px] text-left text-white h-[85px] sm:h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise">{t.faq.table3row4}</button>
-    {stateAction[3][0] ? (
-    <div className="pl-[20px] h-[110px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center ">
-    <p className="text-white">{t.faq.table3text4} <a className="text-cerise" href="mailto:sales@ddagen.se">sales@ddagen.se</a></p>
-    </div>) : null}
-</div>)
-}
-
-function Table4(stateAction: Array<[boolean, React.Dispatch<React.SetStateAction<boolean>>]>){
-  const t = useLocale()
-  return(
-  <div className="mt-[50px]">
-
-    <button onClick={() => stateAction[0][1](!stateAction[0][0])} id="accordion" className="pl-[20px] text-left text-white h-[130px] sm:h-[55px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise"> {t.faq.table4row1}</button>
-    {stateAction[0][0] ? (
-    <div className="pl-[20px] h-[350px] sm:h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center ">
-    <p className="text-white">{t.faq.table4text1} <a className="text-cerise" href="mailto:alexandre.moch@ddagen.se">alexandre.moch@ddagen.se</a></p>
-    </div>) : null}
+    {questions.map((_, i) => (
+      <div>
+        <button onClick={() => 
+          stateAction[i][1](!stateAction[i][0])} className="pl-[20px] pr-[20px] py-[15px] text-left text-white max-h-[300px] w-full bg-slate-50 bg-opacity-20 border-[3px] border-cerise"> {questions[i]}
+        </button>
+      {stateAction[i][0] ? (
+      <div className="pl-[20px] pr-[20px] max-h-[300px] min-h-[100px] bg-gray bg-opacity-50 border-[1px] border-cerise flex items-center">
+      <p className=" py-[20px] text-white">{answers[i]}</p>
+      </div>) : null}
+      </div>
+    ))}   
     
-</div>)
+    </div>
+
+  )
 }
 
 export default function Faq() {
+
   const t = useLocale();
   const slides = [
     {
@@ -112,13 +38,16 @@ export default function Faq() {
     },
   ];
 
-  const tables = [Table1([useState(false), useState(false), useState(false)]), Table2([useState(false), useState(false)]), Table3([useState(false), useState(false), 
-  useState(false), useState(false)]), Table4([useState(false)])]
+  const tables = 
+  [
+    Table([t.faq.table1row1, t.faq.table1row2, t.faq.table1row3], [t.faq.table1text1, t.faq.table1text2, t.faq.table1text3], Array.from({ length: 3 }, () => useState(false))), 
+    Table([t.faq.table2row1, t.faq.table2row2], [t.faq.table2text1, t.faq.table2text2], Array.from({ length: 2 }, () => useState(false))), 
+    Table([t.faq.table3row1, t.faq.table3row2, t.faq.table3row3, t.faq.table3row4], [t.faq.table3text1, t.faq.table3text2, t.faq.table3text3, t.faq.table3text4], Array.from({ length: 4 }, () => useState(false))),
+    Table([t.faq.table4row1], [t.faq.table4text1], [useState(false)])
+  ]
   
   const [currentTable, setCurrentTable] = useState(0);
   const [currentSlide, setCurrentSlide] = useState(0);
-
-  const Tst = tables[0]
 
   return (
     <div className="py-[200px] font-['NeueHaasDisplayRoman'] flex flex-col items-center">
