93dc295bcc4a85076d12ae1e3261d440a3bd1af8
Ernest Umeh <66337956+ernum@users.noreply.github.com>
2023-04-03 16:57:00 +0200

    fix(app): Add spam awareness text to registration form (#178)
    * Add dev deployment on medusa
    
    * Add dev deployment on medusa
    
    * fix: Add spam awareness text to registration form
    
    ---------
    
    Co-authored-by: mathiasmagnusson <mathias@magnusson.space>

--- src/pages/f%C3%B6retagsanm%C3%A4lan.tsx
+++ src/pages/f%C3%B6retagsanm%C3%A4lan.tsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 function RegistrationConfirmation({ t }: { t: Locale["postCompanyForm"] }) {
     return (
         <div className="mx-auto flex flex-col items-center text-center py-40">
-            <h1 className="md:max-w-2xl md:text-5xl mx-10 text-3xl text-cerise uppercase mb-24">{t.title}</h1>
+            <h1 className="md:max-w-2xl md:text-5xl mx-10 text-3xl text-cerise uppercase mb-20">{t.title}</h1>
             <h2 className="text-lg text-white w-2/3 uppercase mb-5">{t.subtitle}</h2>
-            <p className="max-w-[350px] w-2/3 text-[#a7a7a7] mb-14">{t.text} <a href="mailto:sales@ddagen.se" className="text-cerise hover:underline">sales@ddagen.se</a></p>
+            <p className="max-w-[350px] w-2/3 text-[#a7a7a7] mb-14">{t.text} <a href="mailto:sales@ddagen.se" className="text-cerise hover:underline">sales@ddagen.se</a>. {t.textContinuation}</p>
 
             <a href="mailto:sales@ddagen.se" className="
                 bg-cerise transition-transform hover:scale-110
