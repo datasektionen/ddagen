7e8d02c70e1d3d6e18618863d337c5a4ee423622
mathiasmagnusson <mathias@magnusson.space>
2023-04-05 11:59:02 +0200

    Refactor dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import en from "@/locales/en";
-import sv from "@/locales/sv";
 import { useEffect, useState } from "react";
+import { useLocale } from "@/locales";
 
-function NavLink({ href, children, "class": className }: { href: string, children: React.ReactNode, "class": string }) {
+function NavLink({ href, children, "class": className, style }: { href: string, children: React.ReactNode, "class"?: string, style?: React.CSSProperties }) {
   const router = useRouter();
 
   return <Link
-    className={className + " hover:text-cerise" + (router.pathname == encodeURI(href) ? " text-cerise" : "")}
+    className={(className ?? "") + " hover:text-cerise" + (router.pathname == encodeURI(href) ? " text-cerise" : "")}
     href={href}
+    style={style}
   >{children}</Link>;
 }
 
@@ -33,10 +33,45 @@ function Logo({ "class": className }: { "class"?: string }) {
   );
 }
 
+function Group({ links }: { links: { href: string, text: string }[] }) {
+  const [hovered, setHovered] = useState(false);
+  const router = useRouter();
+
+  const selected = links.some(l => router.pathname.startsWith(encodeURI(l.href)));
+  const expanded = hovered || selected;
+
+  return (
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className="lg:ml-10 flex flex-row relative"
+    >
+      <div
+        style={{ height: 70 + links.length * 40 }}
+        className={
+          "hidden lg:block absolute " + (expanded
+            ? "bg-[#666474] bg-opacity-60 rounded-md -top-[50px] -left-[20px] w-[calc(100%+40px)]"
+            : "")
+        }
+      />
+      {links.map(({ href, text }, i) => i == 0
+        ? <NavLink class="z-10 pl-14 lg:pl-0 p-4" href={links[0].href}>{links[0].text}</NavLink>
+        : <NavLink
+          style={{ top: 40 * i }}
+          class={(!expanded ? "lg:hidden" : "") + " z-10 lg:w-full lg:absolute p-4 lg:px-0"}
+          href={href}
+        >{text}</NavLink>)}
+    </div>
+  );
+}
+
 export default function Navbar() {
   const router = useRouter();
-  const { locale } = router;
-  const { nav } = locale === "en" ? en : sv;
+  const l = useLocale();
+  const t = l.nav;
+  const locale = l.locale;
+
+  const [open, setOpen] = useState(false);
 
   function swapLocale() {
     router.push(router.pathname, router.pathname, {
@@ -44,9 +79,6 @@ export default function Navbar() {
       scroll: false,
     });
   }
-
-  const [open, setOpen] = useState(false);
-  const [forCompanyDrop, setForCompanyDrop] = useState(false);
 
   useEffect(() => {
     function close(event: MouseEvent) {
@@ -87,7 +119,7 @@ export default function Navbar() {
         </div>
 
         <div className={`
-          flex justify-between flex-col items-stretch gap-8
+          flex justify-between flex-col items-stretch
           bg-darkblue lg:bg-transparent
           absolute w-full transition-all duration-300
           top-0 pt-20
@@ -98,28 +130,14 @@ export default function Navbar() {
           <a
             className="sr-only focus:not-sr-only"
             href="#main-content"
-          >{nav.toContent}</a>
-          <div className="flex flex-col lg:flex-row items-start h-[100px]">
-            <div className="mr-[40px] mt-4">
-              <NavLink class="px-14 lg:px-0" href="/">{nav.home}</NavLink>
-            </div>
-
-            <div className={`${forCompanyDrop ? 'lg:bg-[#666474] lg:bg-opacity-60 lg:rounded-md lg:w-[200px] w-[420px]' : null} lg:px-4 pb-2`}>
-              <div className="flex flex-row lg:flex-col mt-4 ">
-                <div onMouseEnter={() => setForCompanyDrop(true)} onMouseLeave={() => setForCompanyDrop(false)}>
-                  <NavLink  class="pl-14 pr-4 lg:px-0" href="/förföretag">{nav.forCompanies}</NavLink>
-                </div>
-                <div className="lg:block hidden">
-                  {forCompanyDrop ? (
-                    <div onMouseEnter={() => setForCompanyDrop(true)} onMouseLeave={() => setForCompanyDrop(false)}><NavLink class="pr-14 lg:px-0" href="/faq">faq</NavLink></div>) : null }
-                </div>
-                {open ? (
-                  <div onMouseEnter={() => setForCompanyDrop(true)} onMouseLeave={() => setForCompanyDrop(false)}><NavLink class="pr-14 lg:px-0" href="/faq">faq</NavLink></div>) : null }
-              </div>
-            </div>
-          </div>
-          {/*<NavLink class="px-14 lg:px-0" href="/förstudenter">{nav.forStudents}</NavLink>*/}
-          {/*<NavLink class="px-14 lg:px-0" href="/mässan">{nav.about}</NavLink>*/}
+          >{t.toContent}</a>
+          <NavLink class="px-14 lg:px-4 p-4" href="/">{t.home}</NavLink>
+          <Group links={[
+            { href: "/förföretag", text: t.forCompanies },
+            { href: "/faq", text: "faq" },
+          ]} />
+          {/*<NavLink class="px-14 lg:px-0" href="/förstudenter">{t.forStudents}</NavLink>*/}
+          {/*<NavLink class="px-14 lg:px-0" href="/mässan">{t.about}</NavLink>*/}
           <div className="
             flex flex-row justify-center items-center
             py-4 gap-8
@@ -128,7 +146,7 @@ export default function Navbar() {
             <Link
               className="bg-cerise py-2.5 px-4 rounded-full text-center hover:scale-105 transition-transform"
               href="/företagsanmälan"
-            >{nav.companyForm}</Link>
+            >{t.companyForm}</Link>
             <button data-dont-close onClick={swapLocale} className={`
               w-8 h-8
               rounded-full border-none
@@ -138,7 +156,7 @@ export default function Navbar() {
                 ? "bg-[url(/img/uk-flag.svg)]"
                 : "bg-[url(/img/se-flag.svg)]"
               )}
-            ><img className="sr-only" alt={nav.changeLanguage} /></button>
+            ><img className="sr-only" alt={t.changeLanguage} /></button>
           </div>
         </div>
       </nav>
