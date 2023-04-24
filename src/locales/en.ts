7cf6f7a3431eaafccdfe6ecffc9a9c02bf04a051
rikaznismi <rikaznismi@hotmail.com>
2023-04-25 01:13:52 +0200

    Change height, and push images further to their side on xl:

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -149,7 +149,7 @@ export default {
     p1: "If you have any suggestions for events or activities you want to organize for the students at the Computer Science Chapter, or if you have any other questions or concerns about company packages and the fair,  feel free to contact us at contact us at...",
     p2: "For more information and other questions to those responsible for D-dagen, contact us at...",
     salesHeader: "Sales team",
-    bossesHeader: "D-DAGEN Managers",
+    bossesHeader: "Managers",
     roles: [
       "HEAD OF D-DAGEN",
       "HEAD OF D-DAGEN",
