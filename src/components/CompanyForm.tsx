1045cebc368fcf2be6b4008136685cd9d6a9de02
rikaznismi <rikaznismi@hotmail.com>
2023-04-27 01:23:50 +0200

    Make boldness of title in company form conistent with other

--- src/components/CompanyForm.tsx
+++ src/components/CompanyForm.tsx
@@ -100,7 +100,7 @@ export default function CompanyForm({
 
   return (
     <div className="w-full flex flex-col justify-center items-center py-40">
-      <h1 className="text-center text-3xl md:text-5xl text-cerise uppercase">
+      <h1 className="text-center text-3xl md:text-5xl font-medium text-cerise uppercase">
         {t.companyForm.title}
       </h1>
       <p className="text-center min-w-[100px] max-w-[400px] w-full text-white mt-10"> {t.companyForm.description}</p>
