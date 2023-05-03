07580473de3bfb8a1bc91385e3d5fbec9d5b7233
rikaznismi <rikaznismi@hotmail.com>
2023-05-03 02:19:47 +0200

    fix buttons to be independent of each other

--- src/pages/katalog.tsx
+++ src/pages/katalog.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 
 export default function Catalog() {
     const t = useLocale();
-    const [dropped, setDrop] = useState(false);
     const packets = [t.catalog.basePacket, t.catalog.sponsorPacket, t.catalog.headhHunterPacket, t.catalog.premiumPacket]
     const prices = ["35 000:-", "45 000:-", "55 000:-" , "70 000:-"]
     const iconsBase = ["fa-expand", "fa-laptop", "fa-circle", "fa-users", "fa-ticket-alt", "fa-cocktail", "fa-wifi", "fa-couch", "fa-user-tie", "fa-box-open",]
@@ -20,6 +19,8 @@ export default function Catalog() {
     const packetColor1 = ["border-[#E2B7C9]", "border-pink-300", "border-cerise", "border-yellow"]
     const packetColor2 = ["bg-[#E2B7C9]", "bg-pink-300", "bg-cerise", "bg-yellow"]
 
+    const stateAction = Array.from({ length: packets.length}, () => useState(false));
+
     return (
         <div className="pt-[200px] pb-[600px]">
             <h1 className="uppercase text-cerise text-5xl font-medium text-center">{t.catalog.header}</h1>
@@ -30,22 +31,25 @@ export default function Catalog() {
                         <h1 className="text-white text-center text-3xl">{t.catalog.packetType[0]}</h1>
                         <h1 className="text-yellow text-center text-1xl">{t.catalog.info}</h1>
                         <h1 className="text-white text-center text-7xl py-10">{prices[i]}</h1>
-                        <button onClick={() => setDrop(!dropped)} className={`absolute h-[40px] w-[100px] ${packetColor2[i]} rounded-[40px] border-cerise bottom-0 translate-y-1/2`}>
-                            <i className={`${dropped ? "" : "rotate-180"} duration-200 fas fa-chevron-up text-white`}></i>
+                        <button onClick={() => stateAction[i][1](!stateAction[i][0])} className={`absolute h-[40px] w-[100px] ${packetColor2[i]} rounded-[40px] border-cerise bottom-0 translate-y-1/2`}>
+                            <i className={`${stateAction[i][0] ? "" : "rotate-180"} duration-200 fas fa-chevron-up text-white`}></i>
                         </button>
                     </div>
                     <div className={`
-                    ${dropped ? " max-h-full py-[40px]"
+                    ${stateAction[i][0] ? " max-h-full py-[40px]"
                               : "max-h-0 text-[0px] border-b-0"}
                     duration-200 z-0 flex flex-col px-10 border-[#E2B7C9] border-[3px] border-t-0 rounded-b-xl bg-slate-50 bg-opacity-10 items-center`}>
                     {packet.map((row, j) => (
                             <div className={`flex flex-col py-2 items-center`}>
-                                <i className={`${dropped ? "text-4xl" : "text-[0px]"} fas ${icons[i][j]} duration-200 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}></i>
+                                <i className={`${stateAction[i][0] ? "text-4xl" : "text-[0px]"} fas ${icons[i][j]} duration-200 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}></i>
                                 <p className="text-white">{row}</p>
                             </div>
                         ))}
                     </div>
                 </div>)}
+                
+            </div>
+            <div className="flex flex-row text-white px-[100px] pt-[100px]">
                 
             </div>
         </div>
