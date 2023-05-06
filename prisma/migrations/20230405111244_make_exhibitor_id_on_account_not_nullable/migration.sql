0533d48b4db778453208ca2ae1f18a7f19782b29
mathiasmagnusson <mathias@magnusson.space>
2023-05-06 17:24:00 +0200

    Revert whitespace change in migration

--- prisma/migrations/20230405111244_make_exhibitor_id_on_account_not_nullable/migration.sql
+++ prisma/migrations/20230405111244_make_exhibitor_id_on_account_not_nullable/migration.sql
@@ -1,6 +1,8 @@
 /*
   Warnings:
+
   - Made the column `exhibitorId` on table `accounts` required. This step will fail if there are existing NULL values in that column.
+
 */
 -- DropForeignKey
 ALTER TABLE "accounts" DROP CONSTRAINT "accounts_exhibitorId_fkey";
