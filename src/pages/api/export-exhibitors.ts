2b33aec1feb223d4300d21b4f1e793ac28f1ac71
Mathias Magnusson <mathias@magnusson.space>
2023-05-08 20:20:30 +0200

    feat(#201): return and sort by `createdAt` in the exhibitor export
    * Return the `createdAt` field and order by it
    * Add documentation about the sheet script

--- src/pages/api/export-exhibitors.ts
+++ src/pages/api/export-exhibitors.ts
@@ -10,14 +10,16 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const authHeader = req.headers["authorization"];
-  if (!authHeader || authHeader.length != exportToken.length) {
-    return res.status(402).end();
-  }
-  if (!timingSafeEqual(Buffer.from(authHeader), exportToken)) {
+  if (!authHeader ||
+    authHeader.length != exportToken.length ||
+    !timingSafeEqual(Buffer.from(authHeader), exportToken)
+  ) {
     return res.status(402).end();
   }
 
-  const exhibitors = await prisma.exhibitorInterestRegistration.findMany();
+  const exhibitors = await prisma.exhibitorInterestRegistration.findMany({
+    orderBy: { createdAt: "asc" },
+  });
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(exhibitors.map(e => ({
@@ -26,5 +28,41 @@ export default async function handler(
     contactPerson: e.contactPerson,
     phoneNumber: e.phoneNumber,
     email: e.email,
+    createdAt: e.createdAt,
   }))));
 }
+
+/**
+This function is meant to export the exhibitor registrations so that sales can see them.
+In 2023, we made a google sheet for this, which had the following in a Google Apps Script:
+
+```js
+function importExhibitors() {
+  var activeSheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = activeSheet.getSheetByName("Utställare");
+
+  const res = UrlFetchApp.fetch("https://ddagen.se/api/export-exhibitors", {
+    method: "GET",
+    headers: {
+      'Authorization': <whatever env.EXPORT_TOKEN is>,
+    },
+  });
+
+  const data = JSON.parse(res.getContentText());
+
+  sheet.getRange(1, 1, 1, 7).setValues([[
+    "Företagsnamn", "Organisationsnummer", "Kontaktperson", "Telefonnummer",
+    "E-postadress", "Datum", "Anteckningar (kommer ej röras av skript). Senast uppdaterad " + new Date().toLocaleString("se"),
+  ]]).setFontWeight("bold");
+
+  sheet.getRange(2, 1, data.length, 6).setValues(data.map(row => [
+    row.name, "'" + row.organizationNumber, row.contactPerson, "'" + row.phoneNumber, row.email, row.createdAt,
+  ])).setBackground('#eee');
+  sheet.getRange(2 + data.length, 1, 1, 6).setBackground('orange');
+
+  sheet.autoResizeColumns(1, 7);
+}
+```
+
+Which we put on a trigger to run every so often.
+ */
