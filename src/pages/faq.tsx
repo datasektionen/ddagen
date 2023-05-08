835ea4e2c67259bfb133b3002b65923458de9d87
mathiasmagnusson <mathias@magnusson.space>
2023-05-08 21:37:26 +0200

    Fix borders with pseudo-elements (also called cheating)

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -31,12 +31,11 @@ function Table(questions: Array<string>, answers: Array<string>) {
   return (
     <div className="mt-[50px]">
       {questions.map((_, i) => (
-        <div key={i}>
+        <div key={i} className="text-white border-x-2 border-cerise pb-[2px]">
           <button
             onClick={() => stateAction[i][1](!stateAction[i][0])}
-            className={`${i == questions.length-1 ? "border-b-2" : ""} ${i == 0 ? "" : ""} items-center flex flex-row  justify-between pl-[20px] pr-[20px] py-[15px] text-left text-white max-h-[300px] w-full bg-slate-50 bg-opacity-20 border-cerise border-t-2 border-x-2`}
+            className={`${i == 0 ? "border-t-2" : ""} items-center flex flex-row  justify-between pl-[20px] pr-[20px] py-[15px] text-left max-h-[300px] w-full bg-slate-50 bg-opacity-20 border-cerise`}
           >
-            {" "}
             {questions[i]}
             <img
               src="/img/caret.svg"
@@ -46,14 +45,21 @@ function Table(questions: Array<string>, answers: Array<string>) {
             ></img>
           </button>
 
-          <div
-            className={`${
-              stateAction[i][0]
-                ? "py-[40px] border-t-2"
-                : "text-[0px] border-[0px]"
-            }  ${i == questions.length-1 && stateAction[i][0] ? "border-b-2 border-t-0" : ""} transition-[padding-top,padding-bottom] text-white py-[0px] pl-[20px] pr-[20px] bg-gray bg-opacity-50 flex items-center border-cerise border-x-2`}
-          >
-            {handleAnchorStrings(answers[i])}
+          <div className={`${
+            stateAction[i][0]
+              ? "py-10 grid-rows-[1fr]"
+              : "grid-rows-[0fr]"
+            } grid transition-[padding,grid-template-rows] px-5
+            bg-gray bg-opacity-50 border-cerise
+            relative
+            before:bg-cerise after:bg-cerise
+            before:h-[2px] after:h-[2px]
+            before:right-0 after:right-0
+            before:left-0 after:left-0
+            before:top-0 after:bottom-[-2px]
+            before:absolute after:absolute
+          `}>
+            <div className="overflow-hidden">{handleAnchorStrings(answers[i])}</div>
           </div>
         </div>
       ))}
