e0f204214092f82bfea786bf7b24eccab538806c
rikaznismi <rikaznismi@hotmail.com>
2023-06-04 18:35:52 +0200

    Mobile version finished

--- src/pages/event.tsx
+++ src/pages/event.tsx
@@ -38,22 +38,26 @@ function SingleEvent({
   return (
     <div
       className={`${
-        toReverse ? "flex-row-reverse" : "flex-row"
-      } flex flex-row gap-[100px] px-[0px] mt-[100px] justify-center`}
+        toReverse
+          ? "md:flex-row-reverse flex-col-reverse"
+          : "md:flex-row flex-col-reverse"
+      } flex flex-row gap-[50px] px-[50px] md:px-[50px] mt-[100px] justify-center`}
     >
       <div className="py-[0px]">
-        <img src={image} className="h-[300px]"></img>
+        <img src={image} className="md:h-[300px] lg:h-[300px]"></img>
       </div>
       <div
-        className={`${bgColor} ${borderColor} border-[3px] rounded-lg py-[40px] px-[20px] bg-opacity-10 w-[400px] h-[300px]`}
+        className={`${bgColor} ${borderColor} border-[3px] rounded-lg py-[25px] md:py-[25px] lg:py-[50px] px-[20px] bg-opacity-10 md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[300px]`}
       >
-        <h2 className="text-center text-3xl text-white">{eventInfo[0]}</h2>
-        <h3 className="text-center text-cerise text-xl mt-2">
+        <h2 className="text-center lg:text-3xl md: text-xl text-white">
+          {eventInfo[0]}
+        </h2>
+        <h3 className="text-center text-cerise  lg:text-xl md:mt-1 lg:mt-2">
           {" "}
           {t.event.subheader + price + " :-"}
         </h3>
-        <p className="text-white text-start mt-5">{eventInfo[1]}</p>
-        <button className={`${textColor} text-start mt-5`} onClick={openModal}>
+        <p className="text-white text-start mt-5 text-sm">{eventInfo[1]}</p>
+        <button className={`${textColor} text-start mt-3`} onClick={openModal}>
           <u>{t.event.extra}</u>
         </button>
         {modalState && (
@@ -62,9 +66,18 @@ function SingleEvent({
             ref={modalRef}
             onClick={handleOverlayClick}
           >
-            <div className={` bg-white bg-opacity-70 w-[500px] pb-5 flex flex-col  rounded-3xl`}>
-              <div className="py-[0px] justify-center flex flex-row">
-                <img src={image} alt="Modal Image" />
+            <div
+              className={` bg-white bg-opacity-70 w-[500px] pb-5 flex flex-col  rounded-3xl`}
+            >
+              <div className="py-[0px] justify-center flex flex-row relative">
+                <img src={image} />
+                <button
+                  className="absolute top-5 right-10"
+                  onClick={closeModal}
+                >
+                  <div className=" absolute h-[50px] w-[5px] bg-white rounded-md rotate-45"></div>
+                  <div className=" absolute h-[50px] w-[5px] bg-white rounded-md -rotate-45"></div>
+                </button>
               </div>
               <div className="px-5 mt-5">
                 <h2 className="text-center text-3xl text-black">
@@ -94,7 +107,11 @@ export default function Students() {
         borderColor="border-[#E2B7C9]"
         textColor="text-[#E2B7C9]"
         image="/img/lunchPic.png"
-        eventInfo={[t.event.header1, t.event.paragraph1, t.event.fullParagraph1]}
+        eventInfo={[
+          t.event.header1,
+          t.event.paragraph1,
+          t.event.fullParagraph1,
+        ]}
         price="50 000"
       />
       <SingleEvent
@@ -103,7 +120,11 @@ export default function Students() {
         toReverse={true}
         textColor="text-[#D5759C]"
         image="/img/officePic.png"
-        eventInfo={[t.event.header2, t.event.paragraph2, t.event.fullParagraph2]}
+        eventInfo={[
+          t.event.header2,
+          t.event.paragraph2,
+          t.event.fullParagraph2,
+        ]}
         price="15 000"
       />
       <SingleEvent
@@ -111,7 +132,11 @@ export default function Students() {
         borderColor="border-cerise"
         textColor="text-cerise"
         image="/img/barPic.png"
-        eventInfo={[t.event.header3, t.event.paragraph3, t.event.fullParagraph3]}
+        eventInfo={[
+          t.event.header3,
+          t.event.paragraph3,
+          t.event.fullParagraph3,
+        ]}
         price="60 000"
       />
       <SingleEvent
@@ -120,7 +145,11 @@ export default function Students() {
         toReverse={true}
         textColor="text-yellow"
         image="/img/afterworkPic.png"
-        eventInfo={[t.event.header4, t.event.paragraph4, t.event.fullParagraph4]}
+        eventInfo={[
+          t.event.header4,
+          t.event.paragraph4,
+          t.event.fullParagraph4,
+        ]}
         price="17 000 kr + 150 kr / student"
       />
     </div>
