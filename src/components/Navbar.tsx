bd076691f3c43dae1cb2fa9d7f84bcc73e6b5ab5
rikaznismi <58601215+rikaznismi@users.noreply.github.com>
2023-06-06 01:02:11 +0200

    feat(app): Event page (#209)
    * Begin work on event page
    
    * finish desktop version except support for readmore
    
    * Add correct prices
    
    * Add correct color for each event
    
    * Add modal
    
    * Desktop version finished
    
    * Mobile version finished
    
    * Add event to navbar
    
    * Fix the 'click box' size
    
    * Fix grammar error
    
    * Pass all arguments for every instances 'singleEvent'
    
    * Make compiler happy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -179,6 +179,7 @@ export default function Navbar() {
             <Group links={[
               { href: "/förföretag", text: t.forCompanies },
               { href: "/katalog", text:t.catalog  },
+              { href: "/event", text: "event" },
               { href: "/faq", text: "faq" },
               // isLoggedIn.data == true ?
               //   { href: "/utställare", text: t.exhibitorSettings } :
