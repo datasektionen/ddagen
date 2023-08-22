7d94dc0224cfddcc845228bd0fc7b25badc06d91
rikaznismi <58601215+rikaznismi@users.noreply.github.com>
2023-08-22 14:38:42 +0200

    fix(app): Fix typos (#229)

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -8,12 +8,12 @@ export default {
     introFirstBlock:
       "It’s once again time for D-Dagen, the largest job fair for Computer Science students in the Nordics.",
     introSecondBlock:
-      "Are you studing computer science or IT and looking for your future work place, or do you represent a company and are looking for talented co-workers? In that case, D-Dagen is the perfect opportunity for you to meet people from the industry and expand your network with valuable contacts! We’re striving to make this years fair the largest so far, with over 100 exhibitors and 3000 participants.",
+      "Are you studying Computer Science or IT and looking for your future work place, or do you represent a company and are looking for talented co-workers? In that case, D-Dagen is the perfect opportunity for you to meet people from the industry and expand your network with valuable contacts! We’re striving to make this year's fair the largest so far, with over 100 exhibitors and 3000 participants.",
     introSignOff: "Looking forward to meeting you!",
     introDDA: "Axel Johansson & Johan Abdi, Project Managers",
     representative: "Company Representative?",
     representativeDescription:
-      "Do you want to reach thousands of civil engineering students at KTH? As an exhibitor at D-Dagen you have thet opportunity to do so! Click down bellow to read more and register your interest.",
+      "Do you want to reach thousands of Computer Science Engineering students at KTH? As an exhibitor at D-Dagen you have the opportunity to do so! Click the button below to read more and make an application of interest .",
     representativeButton: "Interest Registration",
     stats: {
       firstPart: "100+ exhibitors",
