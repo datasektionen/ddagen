58a38f6650ca88edaae1b32f3611fe1b868d8bfa
Ernest Umeh <66337956+ernum@users.noreply.github.com>
2023-09-05 12:33:33 +0200

    feat(app): Add amount of exhibitors to admin page (#233)

--- src/locales/sv.ts
+++ src/locales/sv.ts
@@ -482,6 +482,7 @@ export default {
       password: "Lösenord",
     },
     sales: {
+      amountOfExhibitors: "Antalet Utställare",
       header: {
         title: "Utställare",
         name: "Namn",
