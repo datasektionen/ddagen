c5e7ed3058fe285025af35c549854ea8c1a0c994
Ernest Umeh <umeh@kth.se>
2023-09-10 17:59:48 +0200

    fix: Migration error

--- prisma/migrations/20230908213018_add_new_exhibitor_packages/migration.sql
+++ prisma/migrations/20230908213018_add_new_exhibitor_packages/migration.sql
@@ -5,6 +5,11 @@
 -- multiple migrations, each migration adding only one value to
 -- the enum.
 
-
-ALTER TYPE "Package" ADD VALUE 'main';
-ALTER TYPE "Package" ADD VALUE 'startup';
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Package_new" AS ENUM ('main', 'base', 'sponsor', 'premium', 'startup', 'headhunter');
+ALTER TABLE "exhibitors" ALTER COLUMN "package" TYPE "Package_new" USING ("package"::text::"Package_new");
+ALTER TYPE "Package" RENAME TO "Package_old";
+ALTER TYPE "Package_new" RENAME TO "Package";
+DROP TYPE "Package_old";
+COMMIT;
