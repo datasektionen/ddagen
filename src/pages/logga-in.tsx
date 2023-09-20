d39daa25d03df7fe3951deebe31b81e9e495e4ca
Ernest Umeh <umeh@kth.se>
2023-09-21 00:33:33 +0200

    fix: Redirect to exhibitor page if logged in

--- src/pages/logga-in.tsx
+++ src/pages/logga-in.tsx
@@ -4,13 +4,7 @@ import { api } from "@/utils/api";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-function Submit({
-  value,
-  loading,
-}: {
-  value: string;
-  loading: boolean;
-}) {
+function Submit({ value, loading }: { value: string; loading: boolean }) {
   return (
     <input
       type="submit"
@@ -31,13 +25,23 @@ export default function Login() {
   const locale = t.locale;
   const trpc = api.useContext();
 
+  const [code, setCode] = useState("");
+  const [email, setEmail] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>();
   const [state, setState] = useState<"email" | "code">("email");
 
-  const [email, setEmail] = useState("");
   const startLogin = api.account.startLogin.useMutation();
-
-  const [code, setCode] = useState("");
   const finishLogin = api.account.finishLogin.useMutation();
+  const getIsLoggedIn = api.account.isLoggedIn.useQuery(undefined, {
+    onSuccess: (data) => {
+      setIsLoggedIn(data);
+    },
+  });
+
+  useEffect(() => {
+    if (!getIsLoggedIn.isSuccess) return;
+    if (isLoggedIn == true) router.push("/utst%C3%A4llare");
+  }, [isLoggedIn]);
 
   useEffect(() => {
     if (startLogin.isSuccess) {
@@ -62,45 +66,66 @@ export default function Login() {
 
   return (
     <div className="mx-auto flex flex-col items-center text-center mb-40">
-      <h1 className="text-cerise pt-[200px] mb-16 text-5xl font-medium uppercase">{t.login.title}</h1>
-      {state === "email" ? <>
-        <form className="flex flex-col gap-6" onSubmit={(e) => {
-          e.preventDefault();
-          startLogin.mutate({ email, locale });
-        }}>
-          <InputField
-            name="email"
-            value={email}
-            type="email"
-            setValue={setEmail}
-            fields={{ email: t.login.email }}
-          />
-          <p className="text-white max-w-xs">{t.login.emailText}</p>
-          <Submit value={t.login.confirm} loading={startLogin.isLoading} />
-        </form>
-        {startLogin.error && (
-          <p className="text-red-500 font-bold mt-6">{t.error.unknown}</p>
-        )}
-      </> : <>
-        <form className="flex flex-col gap-6 items-center" onSubmit={(e) => { e.preventDefault(); finishLogin.mutate(code); }}>
-          <InputField
-            name="code"
-            value={code}
-            type="text"
-            setValue={setCode}
-            fields={{ code: t.login.confirmationCode }}
-            class="w-96"
-          />
-          <p className="text-white max-w-xs">{t.login.confirmationCodeText1}<span className="font-bold">{email}</span>{t.login.confirmationCodeText2}</p>
-          <Submit value={t.login.confirm} loading={finishLogin.isLoading} />
-        </form>
-        {finishLogin.data?.error && (
-          <p className="text-red-500 font-bold mt-6">{t.error[finishLogin.data.error]}</p>
-        )}
-        {finishLogin.error && (
-          <p className="text-red-500 font-bold mt-6">{t.error.unknown}</p>
-        )}
-      </>}
+      <h1 className="text-cerise pt-[200px] mb-16 text-5xl font-medium uppercase">
+        {t.login.title}
+      </h1>
+      {state === "email" ? (
+        <>
+          <form
+            className="flex flex-col gap-6"
+            onSubmit={(e) => {
+              e.preventDefault();
+              startLogin.mutate({ email, locale });
+            }}
+          >
+            <InputField
+              name="email"
+              value={email}
+              type="email"
+              setValue={setEmail}
+              fields={{ email: t.login.email }}
+            />
+            <p className="text-white max-w-xs">{t.login.emailText}</p>
+            <Submit value={t.login.confirm} loading={startLogin.isLoading} />
+          </form>
+          {startLogin.error && (
+            <p className="text-red-500 font-bold mt-6">{t.error.unknown}</p>
+          )}
+        </>
+      ) : (
+        <>
+          <form
+            className="flex flex-col gap-6 items-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+              finishLogin.mutate(code);
+            }}
+          >
+            <InputField
+              name="code"
+              value={code}
+              type="text"
+              setValue={setCode}
+              fields={{ code: t.login.confirmationCode }}
+              class="w-96"
+            />
+            <p className="text-white max-w-xs">
+              {t.login.confirmationCodeText1}
+              <span className="font-bold">{email}</span>
+              {t.login.confirmationCodeText2}
+            </p>
+            <Submit value={t.login.confirm} loading={finishLogin.isLoading} />
+          </form>
+          {finishLogin.data?.error && (
+            <p className="text-red-500 font-bold mt-6">
+              {t.error[finishLogin.data.error]}
+            </p>
+          )}
+          {finishLogin.error && (
+            <p className="text-red-500 font-bold mt-6">{t.error.unknown}</p>
+          )}
+        </>
+      )}
     </div>
   );
 }
