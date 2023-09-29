af867007e1ca60a97c4a7fa217c66aaf2dcf7063
Ernest Umeh <umeh@kth.se>
2023-09-29 23:32:43 +0200

    fix(app): Disable registration

--- src/pages/f%C3%B6retagsanm%C3%A4lan.tsx
+++ src/pages/f%C3%B6retagsanm%C3%A4lan.tsx
@@ -2,9 +2,22 @@ import CompanyForm from "@/components/CompanyForm";
 import { type Locale, useLocale } from "@/locales";
 import { useState } from "react";
 
+function NoMoreRegistrations({ t }: { t: Locale }) {
+  return (
+    <div className="flex flex-col items-center text-center pt-[200px] pb-40 mx-auto">
+      <h1 className="uppercase text-center text-3xl md:text-5xl mb-14 font-medium text-cerise">
+        {t.companyForm.title}
+      </h1>
+      <p className="max-w-[350px] w-2/3 text-[#a7a7a7]">
+        {t.companyForm.noMoreRegistrations}
+      </p>
+    </div>
+  );
+}
+
 function RegistrationConfirmation({ t }: { t: Locale }) {
   return (
-    <div className="mx-auto flex flex-col items-center text-center pt-[200px] pb-40">
+    <div className="flex flex-col items-center text-center pt-[200px] pb-40 mx-auto">
       <h1 className="uppercase text-center text-3xl md:text-5xl mb-14 font-medium text-cerise">
         {t.companyForm.title}
       </h1>
@@ -44,10 +57,17 @@ export default function ExhibitorRegistration() {
 
   return (
     <>
-      {registationDone ? (
-        <RegistrationConfirmation t={t} />
+      {acceptRegistrations ? (
+        registationDone ? (
+          <RegistrationConfirmation t={t} />
+        ) : (
+          <CompanyForm
+            t={t}
+            onRegistationDone={() => setRegistationDone(true)}
+          />
+        )
       ) : (
-        <CompanyForm t={t} onRegistationDone={() => setRegistationDone(true)} />
+        <NoMoreRegistrations t={t} />
       )}
     </>
   );
