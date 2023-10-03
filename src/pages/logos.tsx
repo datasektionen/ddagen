b6bd5d1266d3d04b1e1fc283977421631021d146
rikaznismi <rikaznismi@hotmail.com>
2023-10-03 13:35:36 +0200

    merge packets with same display values to avoid, elements alone in a row

--- src/pages/logos.tsx
+++ src/pages/logos.tsx
@@ -173,7 +173,8 @@ export default function Logos({ exhibitorData }: LogosProps) {
   const startupPackages = exhibitorData.filter(
     (e) => e.package === "startup" && e.logoColor
   );
-
+  const baseAndStartUpPackages = [...basePackages, ...startupPackages];
+  const sponsorAndHeadhunterAndPremiumPackages = [...headhunterAndPremiumPackages, ...sponsorPackages];
   return (
     <div className="pt-[200px] pb-[300px] px-[10px] sm:px-[100px] lg:px-[200px]">
       <h1 className="uppercase text-cerise text-5xl font-medium text-center">
@@ -183,26 +184,22 @@ export default function Logos({ exhibitorData }: LogosProps) {
       <div className="bg-slate-600 py-[50px] mt-[100px] rounded-lg bg-opacity-40 px-[50px]">
       <div className="block sm:hidden">
         {RenderLogos(headsponsorPackages, 1, "w-[250px]")}
-        {RenderLogos(headhunterAndPremiumPackages, 2, "w-[125px]")}
-        {RenderLogos(sponsorPackages, 2, "w-[125px]")}
-        {RenderLogos(basePackages, 3, "w-[80px]")}
-        {RenderLogos(startupPackages, 3, "w-[80px]")}
+        {RenderLogos(sponsorAndHeadhunterAndPremiumPackages, 2, "w-[125px]")}
+        {RenderLogos(baseAndStartUpPackages, 3, "w-[80px]")}
+
       </div>
 
       <div className="hidden xl:block">
         {RenderLogos(headsponsorPackages, 1, "w-[350px]")}
-        {RenderLogos(headhunterAndPremiumPackages, 3, "w-[250px]")}
-        {RenderLogos(sponsorPackages, 3, "w-[250px]")}
-        {RenderLogos(basePackages, 6, "w-[125px]")}
-        {RenderLogos(startupPackages, 6, "w-[125px]")}
+        {RenderLogos(sponsorAndHeadhunterAndPremiumPackages, 3, "w-[250px]")}
+        {RenderLogos(baseAndStartUpPackages, 6, "w-[125px]")}
       </div>
 
       <div className="hidden sm:block xl:hidden">
         {RenderLogos(headsponsorPackages, 1, "w-[350px]")}
-        {RenderLogos(headhunterAndPremiumPackages, 3, "w-[250px]")}
-        {RenderLogos(sponsorPackages, 3, "w-[250px]")}
-        {RenderLogos(basePackages, 4, "w-[125px]")}
-        {RenderLogos(startupPackages, 4, "w-[125px]")}
+        {RenderLogos(sponsorAndHeadhunterAndPremiumPackages, 3, "w-[250px]")}
+        {RenderLogos(baseAndStartUpPackages, 4, "w-[125px]")}
+
       </div>
 
       </div>
