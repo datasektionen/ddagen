6e9370d41792e1039a55f833d26b7d0ea9f74693
ilmal <nils@u1.se>
2024-02-25 11:47:18 +0100

    Beginning studentGetData logic

--- src/pages/logged-in.tsx
+++ src/pages/logged-in.tsx
@@ -9,7 +9,16 @@ const set_cookies = (loginToken: string) => {
 export default function LoggedInPage() {
 
     const studentVerify = api.student.verify.useMutation();
+    const studentGetData = api.student.getData.useMutation();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [ugkthid, setUgkthid] = useState("");
+    
+    const [studentAccoundExists, setStudentAccoundExists] = useState(false);
+
+    // variables from verify, used to prefill the form for students
+    let email = "";
+    let first_name = "";
+    let last_name = "";
 
     useEffect(() => {
         const params: URLSearchParams = new URL(window.location.href).searchParams;
@@ -18,7 +27,7 @@ export default function LoggedInPage() {
 
         // If no login_token in url, check if login token in cookies
         if ((!loginToken || loginToken === "null") && document.cookie.includes("login_token")) {
-            console.log("Checking cookies!")
+            console.log("Checking cookies!");
             const match = document.cookie.toString().match(/login_token=([^;]*)/);
             if (match !== null) {
                 loginToken = match[1];
@@ -32,19 +41,51 @@ export default function LoggedInPage() {
 
         studentVerify.mutateAsync(loginToken)
         .then((res) =>{
-            setIsLoggedIn(res)
-            set_cookies(loginToken)
+            if (res){
+                // update prefill variables
+                const res_json = JSON.parse(res);
+                email = res_json.emails;
+                first_name = res_json.first_name;
+                last_name = res_json.last_name;
+                setUgkthid(res_json.ugkthid);
+                console.log("IT_I", res_json.ugkthid)
+            }
+            setIsLoggedIn(res? true:false);
+            set_cookies(loginToken);
         });
    
     }, []);
+
+    useEffect(()=>{
+        // Get student data, if not all data is fond in the database the response body will be false,
+        // If response body is false, let the user fill out the information form
+        if (ugkthid === "") return
+
+        studentGetData.mutateAsync(ugkthid)
+        .then((res)=>{
+            if (!res) setStudentAccoundExists(false);
+
+            console.log("RESPONSE: ", res);
+        });
+
+    }, [isLoggedIn, ugkthid]);
+
 
     return (
         <>
         {
             isLoggedIn?
-            <p className="h-screen flex items-center justify-center text-green-500">
-                You are logged in! :)
-            </p>
+            <div className="h-screen flex flex-col justify-center items-center">
+                <p className="text-green-500">
+                    You are logged in! :)
+                </p>
+                {
+                    studentAccoundExists? 
+                    <p className="text-white" >your account exists!</p>
+                    :
+                    <p className="text-white" >Create your account</p>
+                }            
+            </div>
             :
             <p className="h-screen flex items-center justify-center text-red-500">
                 You are not logged in! :)
--- src/server/api/routers/student.ts
+++ src/server/api/routers/student.ts
@@ -1,29 +1,45 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { Prisma } from "@prisma/client";
+
 
 export const studentRouter = createTRPCRouter({    
     verify: publicProcedure
     .input(z.string())
     .mutation(async ({ input })=>{
-
         const url = `https://login.datasektionen.se/verify/${input}?api_key=${process.env.LOGIN_API_KEY}`
         try {
             const response = await fetch(url, {
                 method: "GET",
             });
 
-            if (response.status === 200) {
-                console.log("verification successful");
-                return true;
-            } else {
+            if (response.status !== 200) {
                 console.log("verify was not successful");
                 return false;
-            }
+            } 
+
+            console.log("verification successful");
+            return await response.text() 
+
         } catch (error) {
             console.error("Error during verification:", error);
             return false;
         }
+    }),
+    getData: publicProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input })=>{
+        console.log("INPUT: ", input)
+        const student = await ctx.prisma.students.findUnique({
+            where: {
+                ugkthid: input,
+            },
+        });
+        if (student === null) return false
+        
 
     })
+
+
 
 });
