d159680ea3345520fb79c9b19239cde92311525e
ivbq <ivarbo@kth.se>
2024-04-21 12:31:59 +0200

    Add markers and remove built-in nodes

--- src/components/Map/NewMap.tsx
+++ src/components/Map/NewMap.tsx
@@ -1,8 +1,14 @@
 import type Locale from "@/locales";
 import { MapProp } from "@/shared/Classes";
 import { Dispatch } from "react";
-import { ImageOverlay, MapContainer } from 'react-leaflet';
+import { ImageOverlay, MapContainer, Marker } from 'react-leaflet';
+import { DivIcon } from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+const ceriseMarker = new DivIcon({
+  className: 'rounded-full bg-pink-600',
+  iconSize: [30, 30]
+});
 
 export default function Map({
     t,
@@ -26,11 +32,18 @@ export default function Map({
             <MapContainer
                 center={[0, 0]}
                 zoom={9}
-                scrollWheelZoom={false}
+                scrollWheelZoom={true}
                 style={{height: '84.5vh', width: '100%'}}
                 className="w-full"
             >
                 <ImageOverlay url="/img/map/floor-2.svg" bounds={[[-1, -1], [1, 1]]}/>
+                <Marker position={[-0.03, -0.05]} icon={ceriseMarker}/>
+                <Marker position={[-0.03, 0.06]} icon={ceriseMarker}/>
+                <Marker position={[-0.03, 0.17]} icon={ceriseMarker}/>
+                <Marker position={[0.1, -0.02]} icon={ceriseMarker}/>
+                <Marker position={[0.1, 0.11]} icon={ceriseMarker}/>
+                <Marker position={[0.12, -0.19]} icon={ceriseMarker}/>
+                <Marker position={[0.24, -0.16]} icon={ceriseMarker}/>
             </MapContainer>
         </div>
     )
