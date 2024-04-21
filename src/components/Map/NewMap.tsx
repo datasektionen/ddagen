5891d19eb621813a12f18dda8d3c8a7dea80566d
ivbq <ivarbo@kth.se>
2024-04-21 15:19:31 +0200

    Add layer control

--- src/components/Map/NewMap.tsx
+++ src/components/Map/NewMap.tsx
@@ -1,8 +1,8 @@
 import type Locale from "@/locales";
 import { MapProp } from "@/shared/Classes";
 import { Dispatch } from "react";
-import { ImageOverlay, MapContainer, Marker } from 'react-leaflet';
-import { DivIcon, LatLngExpression } from "leaflet";
+import { Circle, FeatureGroup, ImageOverlay, LayerGroup, LayersControl, MapContainer, Marker, Popup, Rectangle } from 'react-leaflet';
+import { DivIcon, LatLngExpression, Layer } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const ceriseMarker = (id: string): DivIcon =>
@@ -90,42 +90,54 @@ export default function Map({
       <div className="w-full md:m-2 box-border backdrop-blur-sm border-4 border-pink-600 rounded-2xl">
         <MapContainer
           center={[0, 0]}
+          minZoom={8}
           zoom={9}
+          maxZoom={11}
           scrollWheelZoom={true}
-          style={{height: '84.5vh', width: '100%'}}
-          className="w-full"
+          style={{height: '84.5vh', width: '100%', borderRadius: '0.75rem'}}
+          attributionControl={false}
         >
+          <LayersControl position="bottomright" collapsed={false}>
+            <LayersControl.BaseLayer checked name="Floor 2">
+              <LayerGroup>
+                <ImageOverlay url="/img/map/floor-2.svg" bounds={[[-1, -1], [1, 1]]}/>
+                <Marker position={[-0.03, -0.05]} icon={ceriseMarker("1")}/>
+                <Marker position={[-0.03, 0.06]} icon={ceriseMarker("2")}/>
+                <Marker position={[-0.03, 0.17]} icon={ceriseMarker("3")}/>
+                <Marker position={[0.1, -0.02]} icon={ceriseMarker("4")}/>
+                <Marker position={[0.1, 0.11]} icon={ceriseMarker("5")}/>
+                <Marker position={[0.12, -0.19]} icon={ceriseMarker("6")}/>
+                <Marker position={[0.24, -0.16]} icon={ceriseMarker("7")}/>
+                <Marker position={[0.32, 0.01]} icon={ceriseMarker("8")}/>
+                <Marker position={[0.3, 0.17]} icon={ceriseMarker("9")}/>
+                <Marker position={[0.22, 0.25]} icon={ceriseMarker("10")}/>
+                <Marker position={[0.10, 0.25]} icon={ceriseMarker("11")}/>
+                <Marker position={[0.55, 0.1]} icon={ceriseMarker("12")}/>
+                <Marker position={[0.55, 0.25]} icon={ceriseMarker("13")}/>
+                <Marker position={[0.67, -0.05]} icon={ceriseMarker("14")}/>
+                <Marker position={[0.67, 0.06]} icon={ceriseMarker("15")}/>
+                <Marker position={[0.67, 0.17]} icon={ceriseMarker("16")}/>
+                <Marker position={[0.67, 0.28]} icon={ceriseMarker("17")}/>
+                <Marker position={[0.67, 0.39]} icon={ceriseMarker("18")}/>
+                <Marker position={[0.57, 0.40]} icon={ceriseMarker("19")}/>
+                <Marker position={[0.47, 0.40]} icon={ceriseMarker("20")}/>
+              </LayerGroup>
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Floor 3">
+              <ImageOverlay url="/img/map/floor-3.svg" bounds={[[-1, -1], [1, 1]]}/>
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="KTH Entrance">
+              <ImageOverlay url="/img/map/kth-entrance.svg" bounds={[[-1, -1], [1, 1]]}/>
+            </LayersControl.BaseLayer>
+          </LayersControl>
 
-          <ImageOverlay url="/img/map/floor-2.svg" bounds={[[-1, -1], [1, 1]]}/>
-
-          {Object.entries(exhibitors).map(([id, exhibitor]) => (
+          {/* {Object.entries(exhibitors).map(([id, exhibitor]) => (
             <Marker
               key={id}
               position={positions[exhibitor.position] as LatLngExpression}
               icon={ceriseMarker(id)}
             />
-          ))}
-
-          <Marker position={[-0.03, -0.05]} icon={ceriseMarker("1")}/>
-          <Marker position={[-0.03, 0.06]} icon={ceriseMarker("2")}/>
-          <Marker position={[-0.03, 0.17]} icon={ceriseMarker("3")}/>
-          <Marker position={[0.1, -0.02]} icon={ceriseMarker("4")}/>
-          <Marker position={[0.1, 0.11]} icon={ceriseMarker("5")}/>
-          <Marker position={[0.12, -0.19]} icon={ceriseMarker("6")}/>
-          <Marker position={[0.24, -0.16]} icon={ceriseMarker("7")}/>
-          <Marker position={[0.32, 0.01]} icon={ceriseMarker("8")}/>
-          <Marker position={[0.3, 0.17]} icon={ceriseMarker("9")}/>
-          <Marker position={[0.22, 0.25]} icon={ceriseMarker("10")}/>
-          <Marker position={[0.10, 0.25]} icon={ceriseMarker("11")}/>
-          <Marker position={[0.55, 0.1]} icon={ceriseMarker("12")}/>
-          <Marker position={[0.55, 0.25]} icon={ceriseMarker("13")}/>
-          <Marker position={[0.67, -0.05]} icon={ceriseMarker("14")}/>
-          <Marker position={[0.67, 0.06]} icon={ceriseMarker("15")}/>
-          <Marker position={[0.67, 0.17]} icon={ceriseMarker("16")}/>
-          <Marker position={[0.67, 0.28]} icon={ceriseMarker("17")}/>
-          <Marker position={[0.67, 0.39]} icon={ceriseMarker("18")}/>
-          <Marker position={[0.57, 0.40]} icon={ceriseMarker("19")}/>
-          <Marker position={[0.47, 0.40]} icon={ceriseMarker("20")}/>
+          ))} */}
         </MapContainer>
       </div>
     );
