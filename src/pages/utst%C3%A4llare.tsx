2be7e9cfc435a52f7e5753fb3f005ca50ca6c7e7
Viktor Rönnbacka Nybäck <90602664+viktorrn@users.noreply.github.com>
2024-06-26 17:35:59 +0200

    Update utst%C3%A4llare.tsx
    Remove stuff

--- src/pages/utst%C3%A4llare.tsx
+++ src/pages/utst%C3%A4llare.tsx
@@ -512,20 +512,6 @@ export default function Exhibitor() {
           </div>
           :  
           <>{table}</>}
-     
-          {/* Packages that have the student meeting functionality
-          { [0,1,2,3].includes(exhibitorPackage.tier)  ? 
-          <>
-            <h2 className="text-cerise text-2xl md:text-4xl font-medium text-center pt-12">{t.exhibitorSettings.table.row4.title} </h2>
-            {meetings}
-
-            <CompanyMeetingBooker/>
-
-          </> 
-          
-          
-          : <></> }
-          */}
           
         </div>
       </div>
