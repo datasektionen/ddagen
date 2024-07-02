74b8495a3b8a6262ea3bdefc0ee7d8dca5cc6dda
ivbq <ivarbo@kth.se>
2024-07-02 12:33:50 +0200

    Update checkmark styling (WIP)

--- src/components/Map/Search.tsx
+++ src/components/Map/Search.tsx
@@ -128,9 +128,9 @@ export default function Search({
                       name={`${pos}`}
                       checked={checkmarks[pos]}
                       onClick={() => toggleCheckmarkAndApply(pos)}
-                      className="form-checkbox w-6 h-6 hover:cursor-pointer hover:border-cerise
+                      className="form-checkbox w-6 h-6 hover:cursor-pointer hover:border-yellow
                                     bg-black/25 checked:bg-cerise checked:border-white rounded-lg focus:ring-0
-                                    border-2 border-yellow"
+                                    border-2 border-cerise"
                     />
                   </div>
                 ))}
