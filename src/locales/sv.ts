a8cd089a00e43cb61f17ce8117caba513344a5b9
Nils Malmberg <69248971+ilmal@users.noreply.github.com>
2024-10-10 19:56:36 +0200

    Update sv.ts

--- src/locales/sv.ts
+++ src/locales/sv.ts
@@ -373,7 +373,7 @@ export default {
   companyForm: {
     title: "Intresseanmälan",
     description:
-      "Fyll i formuläret för att registrera ert företag för D-Dagen 2024. OBS! En intresseanmälan är inte bindande.",
+      "Fyll i formuläret för att registrera ert företag för D-Dagen 2025. OBS! En intresseanmälan är inte bindande.",
     fields: {
       name: "Företagsnamn",
       organizationNumber: "Organisationsnummer",
