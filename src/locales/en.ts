7d2b76c01e37f7f81443611f217c97f9ef03004e
rackner <rackner@kth.se>
2025-01-04 15:11:14 +0100

    Formatting emails

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -438,7 +438,7 @@ export default {
       contactPerson: string,
       phoneNumber: string
     ) => `
-        <body style:"font-family:georgia">
+        <div class=main; style=color:blue;font-family:Arial;>
           <p>Hi!</p>
           <p>We are pleased to confirm we have received your exhibitor registration.
           We will contact you during the spring about whether you got a spot at D-Dagen.
@@ -456,7 +456,8 @@ export default {
           <p>Thank you for your registration and we look forward to a successful event.</p>
           <p>Best regards,</p>
           <p>The D-Dagen project group</p>
-        </body>
+          <img src=/img/d-dagen-logo-25-en.svg></img>
+        </div>
     `,
   },
   meeting_email: {
