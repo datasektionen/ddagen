f349fd0a5e2b92d244e4009c1579c986e9b7aa02
David Björklund <divad.bjo@gmail.com>
2025-02-23 14:34:13 +0100

    Fixed links for Product Catalogs

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -913,7 +913,7 @@ export default {
       "We'd be thrilled to collaborate with you, both before and after D-Dagen! Whether you're interested in hosting a lunch lecture, hackathon, or something completely different, we're open to your ideas. For more information, please contact our sponsorship manager at <a className='text-yellow' href='mailto:jamie.groop@ddagen.se'>jamie.groop@ddagen.se</a>.",
     productCatalog: "PRODUCT CATALOG",
     header: "COMPANY FAQ",
-    catalogPath: "/downloadables/Product_Catalog_2025_en.pdf",
+    catalogPath: "https://ddagen.se/downloadables/Product_Catalog_2025_en.pdf",
   },
   contact: {
     header: "Contact",
