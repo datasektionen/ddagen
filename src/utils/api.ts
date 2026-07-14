03d73838b007df15e0b41ccce7e89ae8d33ca140
David Björklund <divad.bjo@gmail.com>
2026-07-14 17:30:36 +0200

    Added APP_URL environment for base url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,6 +13,7 @@ import { type AppRouter } from "@/server/api/root";
 
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.APP_URL) return `${process.env.APP_URL}`; // For explicit deployment environments
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
